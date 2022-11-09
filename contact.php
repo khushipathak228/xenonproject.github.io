1eb20cb9dcd20ed83aaa8dfb8d3e7158b0485a9a
khushipathak228 <98277191+khushipathak228@users.noreply.github.com>
2022-11-10 04:28:06 +0530

    Publishing a car selling website.
    It is a car selling website. It has many affordable and cool cars.

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="homestyle.css">
+</head>
+<body>
+    <section class="contact" id="contact">
+
+        <h1 class="heading"><span>contact</span> us</h1>
+    
+        <div class="row">
+    
+            <form action="">
+                <h3>get in touch</h3>
+                <input type="text" placeholder="name" class="box">
+                <input type="email" placeholder="email" class="box">
+                <input type="tel" placeholder="subject" class="box">
+                <textarea placeholder="message" class="box" cols="30" rows="10"></textarea>
+                <input type="submit" value="send message" name="contact_user" class="btn">
+            </form>
+    
+        </div>
+    
+    </section>
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="script.js"></script>
+</body>
+</html>
